8d87256bb88e822382758ce5e8f9bd6241fd144c
Matt Feenstra <matt.feenstra@move.com>
2013-10-14 12:28:34 -0700

    update

--- hit_php.pl
+++ hit_php.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 
-my $cmd = "wget http://qaz02htp1802/php_info.php -O /dev/null";
+my $cmd = "wget http://qaz02htp2001/php_info.php -O /dev/null";
 
 while(1) {
 	sleep 1;
