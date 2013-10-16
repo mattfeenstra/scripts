109ffd977c1ab58bb59b339dae5abde4e5ad82ee
Matt Feenstra <matt.feenstra@move.com>
2013-10-16 13:43:50 -0700

    update

---
+++ check_es.sh
@@ -0,0 +1,24 @@
+
+echo "curl -XGET http://daz02app8814:9200/_percolator/_count"
+curl -XGET http://daz02app8814:9200/_percolator/_count
+echo ""
+
+echo "curl -XGET http://daz02app8815:9200/_percolator/_count"
+curl -XGET http://daz02app8815:9200/_percolator/_count
+echo ""
+
+echo "curl -XGET http://qaz02app8814:9200/_percolator/_count"
+curl -XGET http://qaz02app8814:9200/_percolator/_count
+echo ""
+
+echo "curl -XGET http://qaz02app8815:9200/_percolator/_count"
+curl -XGET http://qaz02app8815:9200/_percolator/_count
+echo ""
+
+echo "curl -XGET http://paz02app8814:9200/_percolator/_count"
+curl -XGET http://paz02app8814:9200/_percolator/_count
+echo ""
+
+echo "curl -XGET http://paz02app8815:9200/_percolator/_count"
+curl -XGET http://paz02app8815:9200/_percolator/_count
+echo ""
